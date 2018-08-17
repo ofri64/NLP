188f28be3476f892550acf373895dfacb9e45aa3
Ido Calman <calman.ido@gmail.com>
2018-08-17 12:35:04 +0300

    Changes experiments for faster run

--- project/code/experiments.sh
+++ project/code/experiments.sh
@@ -1,45 +1,25 @@
 #!/bin/sh
-python3 main.py hebrew -r -x 10 &&
-python3 main.py hebrew -r -f prontype -x 10 &&
-python3 main.py hebrew -r -f gender -x 10 &&
-python3 main.py hebrew -r -f number -x 10 &&
-python3 main.py hebrew -r -f person -x 10 &&
-python3 main.py hebrew -r -f tense -x 10 &&
-python3 main.py hebrew -r -f abbr -x 10 &&
-python3 main.py hebrew -r -f voice -x 10 &&
-python3 main.py hebrew -r -f hebbinyan -x 10 &&
-python3 main.py hebrew -r -f definite -x 10 &&
-python3 main.py hebrew -r -f case -x 10 &&
-python3 main.py hebrew -r -f verbform -x 10 &&
-python3 main.py hebrew -r -f polarity -x 10 &&
-python3 main.py hebrew -r -f verbtype -x 10 &&
-python3 main.py hebrew -r -f hebsource -x 10 &&
-python3 main.py hebrew -r -f prefix -x 10 &&
-python3 main.py hebrew -r -f mood -x 10 &&
-python3 main.py hebrew -r -f hebexistential -x 10 &&
-python3 main.py hebrew -r -f reflex -x 10 &&
-python3 main.py hebrew -r -f xtra -x 10 &&
-python3 main.py french -r -x 10 &&
-python3 main.py french -r -f gender 10 &&
-python3 main.py french -r -f number 10 &&
-python3 main.py french -r -f person 10 &&
-python3 main.py french -r -f prontype 10 &&
-python3 main.py french -r -f mood 10 &&
-python3 main.py french -r -f tense 10 &&
-python3 main.py french -r -f verbform 10 &&
-python3 main.py french -r -f definite 10 &&
-python3 main.py french -r -f definite 10 &&
-python3 main.py french -r -f numtype 10 &&
-python3 main.py french -r -f poss 10 &&
-python3 main.py french -r -f polarity 10 &&
-python3 main.py french -r -f reflex 10 &&
-python3 main.py french -r -f voice 10 &&
-python3 main.py chinese -r -x 10 &&
-python3 main.py chinese -r -f numtype 10 &&
-python3 main.py chinese -r -f number 10 &&
-python3 main.py chinese -r -f person 10 &&
-python3 main.py chinese -r -f case 10 &&
-python3 main.py chinese -r -f aspect 10 &&
-python3 main.py chinese -r -f polarity 10 &&
-python3 main.py chinese -r -f voice 10 &&
-python3 main.py chinese -r -f mood 10
+python3 main.py french -r -x 3 &&
+python3 main.py french -r -f gender -x 3 &&
+python3 main.py french -r -f number -x 3 &&
+python3 main.py french -r -f person -x 3 &&
+python3 main.py french -r -f prontype -x 3 &&
+python3 main.py french -r -f mood -x 3 &&
+python3 main.py french -r -f tense -x 3 &&
+python3 main.py french -r -f verbform -x 3 &&
+python3 main.py french -r -f definite -x 3 &&
+python3 main.py french -r -f definite -x 3 &&
+python3 main.py french -r -f numtype -x 3 &&
+python3 main.py french -r -f poss -x 3 &&
+python3 main.py french -r -f polarity -x 3 &&
+python3 main.py french -r -f reflex -x 3 &&
+python3 main.py french -r -f voice -x 3 &&
+python3 main.py chinese -r -x 3 &&
+python3 main.py chinese -r -f numtype -x 3 &&
+python3 main.py chinese -r -f number -x 3 &&
+python3 main.py chinese -r -f person -x 3 &&
+python3 main.py chinese -r -f case -x 3 &&
+python3 main.py chinese -r -f aspect -x 3 &&
+python3 main.py chinese -r -f polarity -x 3 &&
+python3 main.py chinese -r -f voice -x 3 &&
+python3 main.py chinese -r -f mood -x 3
