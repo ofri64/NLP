807dbb79f14f86f23e9e11053679be2fce5b5353
Ido Calman <calman.ido@gmail.com>
2018-08-17 14:03:41 +0300

    French experiments

--- project/code/experiments.sh
+++ project/code/experiments.sh
@@ -1,25 +1,16 @@
 #!/bin/sh
-python3 main.py french -r -x 3 &&
-python3 main.py french -r -f gender -x 3 &&
-python3 main.py french -r -f number -x 3 &&
-python3 main.py french -r -f person -x 3 &&
-python3 main.py french -r -f prontype -x 3 &&
-python3 main.py french -r -f mood -x 3 &&
-python3 main.py french -r -f tense -x 3 &&
-python3 main.py french -r -f verbform -x 3 &&
-python3 main.py french -r -f definite -x 3 &&
-python3 main.py french -r -f definite -x 3 &&
-python3 main.py french -r -f numtype -x 3 &&
-python3 main.py french -r -f poss -x 3 &&
-python3 main.py french -r -f polarity -x 3 &&
-python3 main.py french -r -f reflex -x 3 &&
-python3 main.py french -r -f voice -x 3 &&
-python3 main.py chinese -r -x 3 &&
-python3 main.py chinese -r -f numtype -x 3 &&
-python3 main.py chinese -r -f number -x 3 &&
-python3 main.py chinese -r -f person -x 3 &&
-python3 main.py chinese -r -f case -x 3 &&
-python3 main.py chinese -r -f aspect -x 3 &&
-python3 main.py chinese -r -f polarity -x 3 &&
-python3 main.py chinese -r -f voice -x 3 &&
-python3 main.py chinese -r -f mood -x 3
+../../venv/bin/python3 main.py french -r -x 3 &&
+../../venv/bin/python3 main.py french -r -f gender -x 3 &&
+../../venv/bin/python3 main.py french -r -f number -x 3 &&
+../../venv/bin/python3 main.py french -r -f person -x 3 &&
+../../venv/bin/python3 main.py french -r -f prontype -x 3 &&
+../../venv/bin/python3 main.py french -r -f mood -x 3 &&
+../../venv/bin/python3 main.py french -r -f tense -x 3 &&
+../../venv/bin/python3 main.py french -r -f verbform -x 3 &&
+../../venv/bin/python3 main.py french -r -f definite -x 3 &&
+../../venv/bin/python3 main.py french -r -f definite -x 3 &&
+../../venv/bin/python3 main.py french -r -f numtype -x 3 &&
+../../venv/bin/python3 main.py french -r -f poss -x 3 &&
+../../venv/bin/python3 main.py french -r -f polarity -x 3 &&
+../../venv/bin/python3 main.py french -r -f reflex -x 3 &&
+../../venv/bin/python3 main.py french -r -f voice -x 3
