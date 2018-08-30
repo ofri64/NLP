c0d5ffd6487a6beafae5e786ea56e6f05c557fd9
uridabomb <uriavron@gmail.com>
2018-08-31 01:15:44 +0300

    Adds graphs and code to create them

---
+++ project/code/graphs.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+graph_name = 'unseen'
+file_path = './{0}.csv'.format(graph_name)
+
+df = pd.read_csv(file_path).transpose()
+
+plt.figure()
+plt.tight_layout()
+
+df.plot(grid=True, style='.-')
+
+languages = df.index.values
+plt.xticks(range(len(languages)), languages)
+
+plt.ylabel('Accuracy')
+plt.legend(loc='best')
+
+plt.savefig('{0}.png'.format(graph_name))
+plt.show()
+
+
